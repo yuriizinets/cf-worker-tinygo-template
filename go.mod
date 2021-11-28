df02de2925cd3bbeda93cc90205cfb99f8069713
Yurii Zinets <yurii.zinets@icloud.com>
2021-11-28 14:48:20 +0100

    minimize template

--- go.mod
+++ go.mod
@@ -1,8 +1,3 @@
 module cloudflare.worker
 
 go 1.17
-
-require (
-	github.com/google/uuid v1.3.0 // indirect
-	github.com/kyoto-framework/kyoto v0.1.1-0.20211124210253-24e5f7545676 // indirect
-)
--- main.go
+++ main.go
@@ -1,16 +1,12 @@
 package main
 
 import (
-	"fmt"
-
 	"cloudflare.worker/cloudflare"
-	"github.com/google/uuid"
 )
 
 func HandlerIndex(r *cloudflare.Request) *cloudflare.Response {
-	uuid := uuid.New().String()
 	return &cloudflare.Response{
-		Body: []byte(fmt.Sprintf(`{"uuid":"%s"}`, uuid)),
+		Body: []byte(`{"foo":"bar"}`),
 	}
 }
 
